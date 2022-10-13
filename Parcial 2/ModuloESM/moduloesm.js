2acebd3208ec1f81e9207993f7eb1e6e35a1412d
cdlr247 <cdlr01@hotmail.com>
2022-10-12 21:53:41 -0500

    moduloesm

---
+++ Parcial 2/ModuloESM/moduloesm.js
@@ -0,0 +1,19 @@
+//const cadenas = {}
+
+export let pasarMayusculas = (cadena) => {
+    return cadena.toUpperCase();
+}
+
+export let quitarEspacios = (cadena) =>{
+    return cadena.replace(/ /g,"");
+}
+
+export let obtenerLongitud = (cadena) =>{
+    return cadena.length;
+}
+
+//exports.pasarMayusculas=pasarMayusculas;
+//exports.quitarEspacios=quitarEspacios;
+//exports.obtenerLongitud=obtenerLongitud;
+
+//modulo exports.cadenas;
